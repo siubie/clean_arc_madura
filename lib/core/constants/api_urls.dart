2babf740c485ccd90c81cea4b24fb2dcb02d8918
siubie <putraprima@gmail.com>
2025-05-19 07:08:49 +0900

    Update API base URL and implement initial login BLoC structure

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -1,5 +1,5 @@
 class ApiUrls {
-  static const baseURL = 'http://192.168.3.71:8000/api';
+  static const baseURL = 'http://127.0.0.1:8000/api';
   static const register = '$baseURL/auth/register';
   static const userProfile = '$baseURL/users/profile';
   static const login = '$baseURL/auth/login';
